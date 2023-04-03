aafb1cccbbddc19d9d9563928f25348b5e837dd9
shakiz <activities335@gmail.com>
2023-04-03 16:19:14 +0600

    Add file for bappiness analysis

---
+++ data_mining_happiness_analysis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 16:15:16 2023
+
+@author: sakhawathossain
+"""
+
+import pandas as pd
+import numpy as np
+
+dataSet = pd.read_csv("")
