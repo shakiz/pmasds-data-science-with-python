f0950842efc2f0d6d84c5ffeb6523026262d9c23
shakiz <activities335@gmail.com>
2023-05-27 13:18:11 +0600

    Add plot and log all functions

--- thesis_customer_churn.py
+++ thesis_customer_churn.py
@@ -252,6 +252,33 @@ print(classification_report(df_train.Exited,  RF.predict(df_train.loc[:, df_trai
 print(classification_report(df_train.Exited,  XGB.predict(df_train.loc[:, df_train.columns != 'Exited'])))
 
 
+y = df_train.Exited
+X = df_train.loc[:, df_train.columns != 'Exited']
+X_pol2 = df_train_pol2
+auc_log_primal, fpr_log_primal, tpr_log_primal = get_auc_scores(y, log_primal.predict(X),log_primal.predict_proba(X)[:,1])
+auc_log_pol2, fpr_log_pol2, tpr_log_pol2 = get_auc_scores(y, log_pol2.predict(X_pol2),log_pol2.predict_proba(X_pol2)[:,1])
+auc_SVM_RBF, fpr_SVM_RBF, tpr_SVM_RBF = get_auc_scores(y, SVM_RBF.predict(X),SVM_RBF.predict_proba(X)[:,1])
+auc_SVM_POL, fpr_SVM_POL, tpr_SVM_POL = get_auc_scores(y, SVM_POL.predict(X),SVM_POL.predict_proba(X)[:,1])
+auc_RF, fpr_RF, tpr_RF = get_auc_scores(y, RF.predict(X),RF.predict_proba(X)[:,1])
+auc_XGB, fpr_XGB, tpr_XGB = get_auc_scores(y, XGB.predict(X),XGB.predict_proba(X)[:,1])
+
+
+plt.figure(figsize = (12,6), linewidth= 1)
+plt.plot(fpr_log_primal, tpr_log_primal, label = 'log primal Score: ' + str(round(auc_log_primal, 5)))
+plt.plot(fpr_log_pol2, tpr_log_pol2, label = 'log pol2 score: ' + str(round(auc_log_pol2, 5)))
+plt.plot(fpr_SVM_RBF, tpr_SVM_RBF, label = 'SVM RBF Score: ' + str(round(auc_SVM_RBF, 5)))
+plt.plot(fpr_SVM_POL, tpr_SVM_POL, label = 'SVM POL Score: ' + str(round(auc_SVM_POL, 5)))
+plt.plot(fpr_RF, tpr_RF, label = 'RF score: ' + str(round(auc_RF, 5)))
+plt.plot(fpr_XGB, tpr_XGB, label = 'XGB score: ' + str(round(auc_XGB, 5)))
+plt.plot([0,1], [0,1], 'k--', label = 'Random: 0.5')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC Curve')
+plt.legend(loc='best')
+#plt.savefig('roc_results_ratios.png')
+plt.show()
+
+
 
 # data prep pipeline for test data
 def DfPrepPipeline(df_predict,df_train_Cols,minVec,maxVec):
